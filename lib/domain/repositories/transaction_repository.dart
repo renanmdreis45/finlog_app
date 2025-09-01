f4db840caf33dbe9e4e6c3286895dad7f8f63d22
renan martins <renanmdreis@alu.ufc.br>
2025-09-01 15:33:39 -0300

    feat (domain): add transaction repository interface

---
+++ lib/domain/repositories/transaction_repository.dart
@@ -0,0 +1,24 @@
+import 'package:finlog_app/domain/entities/transaction_entity.dart';
+
+abstract class ITransactionRepository {
+  Future<List<TransactionEntity>> getAllTransactions();
+  Future<List<TransactionEntity>> getTransactions({
+    String? category,
+    DateTime? startDate,
+    DateTime? endDate,
+  });
+  Future<List<TransactionEntity>> getTransactionsByLastWeek();
+  Future<void> addTransaction(TransactionEntity transaction);
+  Future<void> updateTransaction(TransactionEntity transaction);
+  Future<void> deleteTransaction(String id);
+  Future<Map<String, double>> getExpensesByCategory({
+    DateTime? startDate,
+    DateTime? endDate,
+  });
+  Future<Map<DateTime, double>> getMonthlyExpensesEvolution({
+    int monthsBack = 1,
+  });
+  Future<Map<DateTime, double>> getWeeklyExpensesEvolution({
+    int weeksBack = 1,
+  });
+}
